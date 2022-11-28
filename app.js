f878f09cc5e4ed39b326f9129b85a68e8423313d
naimur <naimur202163@gmail.com>
2022-11-28 20:52:07 +0600

    done

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
-const express=require("express");
-const app=express();
-const cookieParser=require("cookie-parser");
-const bodyParser=require("body-parser");
-const path="path"
+const express = require("express");
+const app = express();
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const fileUpload = require("express-fileupload");
+const path = "path";
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(fileUpload());
+
+// Routes Imports
+
+module.exports = app;
