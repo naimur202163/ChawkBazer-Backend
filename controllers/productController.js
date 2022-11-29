98195590152048a8d89e89e6b7bdc6fc1aa9ef63
naimur <naimur202163@gmail.com>
2022-11-29 09:11:04 +0600

    all Routes are working

---
+++ controllers/productController.js
@@ -0,0 +1,6 @@
+exports.getAllProducts = async (req, res, next) => {
+  res.status(201).json({
+    success: true,
+    message: "Hello Iam Ripon Bd    ",
+  });
+};
--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const app = require("./app");
-
+const connectDatabase = require("./config/database");
 // Config
 if (process.env.NODE_ENV !== "PRODUCTION") {
   require("dotenv").config({ path: "./config/config.env" });
 }
+
+// Database Connection
+connectDatabase();
+
+
+
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is  running on Port htpss:localhost:${process.env.PORT}`);
