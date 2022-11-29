ca7c0b5a06307c148547e3e3c3904530c32df18b
naimur <naimur202163@gmail.com>
2022-11-29 08:34:13 +0600

    working database

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const app = require("./app");
 
-app.listen(4000, () => {
-  console.log("Server is  running on Port 4000");
+// Config
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  require("dotenv").config({ path: "./config/config.env" });
+}
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is  running on Port htpss:localhost:${process.env.PORT}`);
 });
